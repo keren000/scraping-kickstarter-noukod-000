a0f8f6063b2368ac2790aa7d48c394a555da9dba
Keren-Happuc Fleurimond <keren@noukod.com>
2018-10-19 16:14:48 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,27 +5,6 @@ require 'pry'
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
- kickstarter = Nokogiri::HTML(html)
-
- projects = {}
-
- # Iterate through the projects
- kickstarter.css("li.project.grid_4").each do |project|
-title: project.css("h2.bbcard_name strong a").text
-   projects[project] = {
-     image link: project.css("div.project-thumbnail a img").attribute("src").value,
-     description: project.css("p.bbcard_blurb").text,
-     location: project.css("ul.project-meta span.location-name").text,
-     percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-   }
- end
-
- # return the projects hash
- projects
-end
-
-def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
  
   projects = {}
@@ -43,3 +22,4 @@ def create_project_hash
   # return the projects hash
   projects
 end
+
